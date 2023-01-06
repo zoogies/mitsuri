5facc99cf052cb01633a24558470102e5a799b58
Ryan Zmuda <ryanzmuda@gmail.com>
2023-01-05 19:59:27 -0500

    refactor: simplify ping command

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -3,23 +3,9 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Sends the current websocket ping')
-        .addStringOption(option =>
-            option.setName('type')
-                .setDescription('Ping type selection')
-                .setRequired(true)
-				.addChoices(
-					{ name: 'Heartbeat', value: 'heart' },
-                    { name: 'Roundtrip', value: 'round' },
-				)),
+		.setDescription('Sends the current websocket ping'),
 	async execute(interaction) {
-        type = interaction.options.getString('type')
-        if(type == 'heart'){
-            await interaction.reply(`🥰 Websocket heartbeat: ${interaction.client.ws.ping}ms 🥰`);
-        }
-		else{
-            const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
-            interaction.editReply(`🥰 Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms 🥰`);
-        }
-	},
+        const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+        interaction.editReply(`**>>** Websocket heartbeat: **${interaction.client.ws.ping}ms**\n**>>** Roundtrip latency: **${sent.createdTimestamp - interaction.createdTimestamp}ms**`);
+    }
 };
