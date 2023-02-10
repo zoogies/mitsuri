3e0b2826056e9c2e61203670704b87617ed6135f
Ryan Zmuda <ryanzmuda@gmail.com>
2023-02-10 18:34:48 -0500

    feat: giphy command

---
+++ src/commands/giphy.js
@@ -0,0 +1,88 @@
+// IDEAS:
+// button on bottom to regen for random
+
+const { SlashCommandBuilder } = require('discord.js');
+const axios = require('axios');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('giphy')
+		.setDescription('GIPHY API interface, used for gifs and other media')
+        .addStringOption(option =>
+            option.setName('type')
+                .setDescription('The type of media sent, defaults to gif')
+                .setRequired(false)
+				.addChoices(
+					{ name: 'Gif', value: 'gifs' },
+                    { name: 'Sticker', value: 'stickers' },
+				))
+        .addStringOption(option =>
+            option.setName('endpoint')
+                .setDescription('The type of search request you want to make, defaults to random')
+                .setRequired(false)
+				.addChoices(
+					{ name: 'Random', value: 'random' },
+				))
+        .addStringOption(option =>
+            option.setName('rating')
+                .setDescription('The explicitivity of the result, defaults to rated R')
+                .setRequired(false)
+                .addChoices(
+                    { name: 'G', value: 'r' },
+                    { name: 'PG', value: 'pg' },
+                    { name: 'PG-13', value: 'pg-13' },
+                    { name: 'R', value: 'r' },
+                ))
+        .addStringOption(option =>
+            option
+                .setName('tag')
+                .setRequired(false)
+                .setMaxLength(50)
+                .setDescription('An optional text tag for the search')),
+	async execute(interaction) {
+        const { giphy_key } = require('..');
+
+        // default type to gifs
+        let type = interaction.options.getString('type') ? interaction.options.getString('type') : 'gifs';
+
+        // default endpoint to random
+        let endpoint = interaction.options.getString('endpoint') ? interaction.options.getString('endpoint') : 'random';
+
+        // default rating to r
+        let rating = interaction.options.getString('rating') ? interaction.options.getString('rating') : 'r';
+
+        // default tag to nothing
+        let tag = interaction.options.getString('tag') ? interaction.options.getString('tag').replace(' ','+') : '';
+
+        // build url
+        let url = `https://api.giphy.com/v1/${type}/${endpoint}?api_key=${giphy_key}&tag=${tag}&rating=${rating}`
+
+		const result = await axios.get(url).then(function(response){
+            interaction.reply(response.data['data']['embed_url']);
+        }).catch(function(error){
+            switch (error.response.status) {
+                case 400:
+                    interaction.reply("Your request was formatted incorrectly or missing a required parameter(s).");
+                    break;
+                case 401:
+                    interaction.reply("Your request lacks valid authentication credentials for the target resource.");
+                    break;
+                case 403:
+                    interaction.reply("You aren't authorized to make this request.");
+                    break;
+                case 404:
+                    interaction.reply("The particular GIF or Sticker you are requesting was not found.");
+                    break;
+                case 414:
+                    interaction.reply("The length of the search query cannot exceed 50 characters.");
+                    break;
+                case 429:
+                    interaction.reply("Too many requests, try again later.");
+                    break;
+                default:
+                    interaction.reply("Something went wrong. Please try again later.");
+                    break;
+            }
+        });
+	},
+};
--- src/index.js
+++ src/index.js
@@ -6,27 +6,42 @@ const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 // openai
 const { Configuration, OpenAIApi } = require("openai");
 
-// RUN DEV: NODE_ENV=development node index.js
 // get the env and read from our config accordingly
 const env = process.env.NODE_ENV || 'production';
 let token;
 let configuration;
+let giphy_key;
+
 if(env == 'development'){
+	// header
 	console.log(">> launching in development mode");
 	token = require('./config-dev.json')['token'];
-	console.log(">> TOKEN: "+token);
+	//console.log(">> TOKEN: "+token);
+
+	// openai object
 	configuration = new Configuration({
 		apiKey: require('./config-dev.json')['OPENAI_API_KEY'],
 	});
 	console.log(">> OPENAI_API_KEY LOADED");
+
+	//giphy object
+	giphy_key = require('./config-dev.json')['giphy_key']
+	console.log(">> GIPHY KEY LOADED")
 }
 else{
+	// header
 	console.log(">> launching in production mode");
 	token = require('/usr/mitsuri/config.json')['token'];
+
+	// openai object
 	configuration = new Configuration({
 		apiKey: require('/usr/mitsuri/config.json')['OPENAI_API_KEY'],
 	});
 	console.log(">> OPENAI_API_KEY LOADED");
+
+	// giphy key
+	giphy_key = require('/usr/mitsuri/config.json')['giphy_key']
+	console.log(">> GIPHY KEY LOADED")
 }
 
 // build our client
@@ -36,7 +51,8 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const openai = new OpenAIApi(configuration);
 module.exports = {
 	openai,
-	client
+	client,
+	giphy_key
 }; // allow this openai object to be accessed from our slash commands
 
 client.commands = new Collection();
